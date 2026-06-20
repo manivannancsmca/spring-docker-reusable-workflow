d8ce035020beff9c84c0587c25882e041b5f837b
manivannancsmca <manivannancsmca@gmail.com>
2026-06-20 17:33:09 +0530

    first commit

---
+++ src/main/java/com/spring_docker_reusable_workflow/SpringDockerReusableWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.spring_docker_reusable_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerReusableWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerReusableWorkflowApplication.class, args);
+	}
+
+}
